621bd440aa2cf62a27ab7c18c2f10d78bcead640
Jaweria Waheed Satti <jaweriasatti19@gmail.com>
2026-07-15 19:36:19 +0500

    Stage 3: create with validation

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 
 const tasks = [
@@ -52,6 +54,28 @@ app.get("/tasks/:id", (req, res) => {
     }
 
     res.json(task);
+});
+
+app.post("/tasks", (req, res) => {
+
+    const title = req.body.title;
+
+    if (!title) {
+        return res.status(400).json({
+            error: "Title is required"
+        });
+    }
+
+    const newTask = {
+        id: tasks.length + 1,
+        title: title,
+        done: false
+    };
+
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+
 });
 
 app.listen(PORT, () => {
